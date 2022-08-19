5d4d0342f65afea9d91804df4ad1a58a5dfd64e0
hyelie <hyelie@postech.ac.kr>
2022-08-19 06:24:17 +0000

    Translate Annotation to English in URLProcessor

--- N2T/src/convert/URLProcessor.java
+++ N2T/src/convert/URLProcessor.java
@@ -4,18 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class URLProcessor {
-    // blog.naver.com/{NAVER_ID}/{POST_NUMBER}
-    Pattern rawPattern = Pattern.compile("http[s]?:\\/\\/blog.naver.com\\/([A-za-z0-9]*)\\/([0-9]*)");
-    // blog.naver.com/PostView.naver?blogId={NAVER_ID}&logNo={POST_NUMBER} 형식
-    Pattern processedPattern = Pattern.compile("http[s]?:\\/\\/blog.naver.com\\/PostView.naver\\?blogId=([A-za-z0-9]*)&logNo=([0-9]*)");
+    // regular expression filtering [blog.naver.com/{NAVER_ID}/{POST_NUMBER}] format
+    private Pattern rawPattern = Pattern.compile("http[s]?:\\/\\/blog.naver.com\\/([A-za-z0-9]*)\\/([0-9]*)");
+    // regular expression filtering [blog.naver.com/PostView.naver?blogId={NAVER_ID}&logNo={POST_NUMBER}] format
+    private Pattern processedPattern = Pattern.compile("http[s]?:\\/\\/blog.naver.com\\/PostView.naver\\?blogId=([A-za-z0-9]*)&logNo=([0-9]*)");
 
     /**
-     * 입력 URL이 rawPattern의 형식이라면 가공이 필요하고,
-     * processedPattern의 형식이라면 가공할 필요가 없음.
-     * 그렇지 않다면 잘못된 URL임
+     * If the inputURL is in the format of rawPattern, processing is required.
+     * If the inputURL is in the format of processedPattern, processing is not necessary.
+     * Otherwise, inputURL is invalid URL
      * 
-     * @param : 입력으로 넣는 URL
-     * @return 네이버 블로그 URL 형식에 맞지 않으면 0, 맞지만 가공할 필요가 있으면 1, 맞으면서 가공할 필요가 없으면 2
+     * @param inputURL : Given URL
+     * @return 0 if invalid, 1 if inputURL need to be processed, 2 if inputURL is already processed
      */
     Integer getNaverBlogURLType(String inputURL){
         if(inputURL.matches(this.processedPattern.toString())) return 2;
@@ -24,11 +24,11 @@ public class URLProcessor {
     }
 
     /**
-     * 네이버 블로그 URL을 processedPattern의 형식으로 바꿈
+     * Change Naver Blog URL to processedPattern format
      * 
-     * @param inputURL - 네이버 블로그 URL
-     * @return processedPattern 형식 URL
-     * @throws Exception 네이버 블로그 URL 형식이 아닌 경우 오류 메시지와 같이 throw
+     * @param inputURL - Naver Blog URL
+     * @return processedPattern format URL
+     * @throws Exception If inputURL is not Naver Blog URL format, then throw with error message
      */
     public String getOriginURL(String inputURL) throws Exception{
         Integer urlType = getNaverBlogURLType(inputURL);
