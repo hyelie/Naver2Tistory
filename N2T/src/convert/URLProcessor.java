a439f7c43235d4bb8e0b6d3ef9a6ea477a5b0c18
hyelie <hyelie@postech.ac.kr>
2022-08-22 11:28:47 +0000

    Refactor Number to Enum in URLProcessor

--- N2T/src/convert/URLProcessor.java
+++ N2T/src/convert/URLProcessor.java
@@ -4,10 +4,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class URLProcessor {
-    // regular expression filtering [blog.naver.com/{NAVER_ID}/{POST_NUMBER}] format
+    // Regular expression filtering [blog.naver.com/{NAVER_ID}/{POST_NUMBER}] format
     private Pattern rawPattern = Pattern.compile("http[s]?:\\/\\/blog.naver.com\\/([A-za-z0-9]*)\\/([0-9]*)");
-    // regular expression filtering [blog.naver.com/PostView.naver?blogId={NAVER_ID}&logNo={POST_NUMBER}] format
+    // Regular expression filtering [blog.naver.com/PostView.naver?blogId={NAVER_ID}&logNo={POST_NUMBER}] format
     private Pattern processedPattern = Pattern.compile("http[s]?:\\/\\/blog.naver.com\\/PostView.naver\\?blogId=([A-za-z0-9]*)&logNo=([0-9]*)");
+    private enum URLType {
+        INVALID,
+        RAW,                    // Need to be processed
+        PROCESSED;              // Already processed
+    }
 
     /**
      * If the inputURL is in the format of rawPattern, processing is required.
@@ -15,12 +20,12 @@ public class URLProcessor {
      * Otherwise, inputURL is invalid URL
      * 
      * @param inputURL : Given URL
-     * @return 0 if invalid, 1 if inputURL need to be processed, 2 if inputURL is already processed
+     * @return enum of URLType. INVALID if invalid, RAW if inputURL need to be processed, PROCESSED if inputURL is already processed
      */
-    Integer getNaverBlogURLType(String inputURL){
-        if(inputURL.matches(this.processedPattern.toString())) return 2;
-        else if(inputURL.matches(this.rawPattern.toString())) return 1;
-        else return 0;
+    URLType getNaverBlogURLType(String inputURL){
+        if(inputURL.matches(this.processedPattern.toString())) return URLType.PROCESSED;
+        else if(inputURL.matches(this.rawPattern.toString())) return URLType.RAW;
+        else return URLType.INVALID;
     }
 
     /**
@@ -31,21 +36,21 @@ public class URLProcessor {
      * @throws Exception If inputURL is not Naver Blog URL format, then throw with error message
      */
     public String getOriginURL(String inputURL) throws Exception{
-        Integer urlType = getNaverBlogURLType(inputURL);
-        if(urlType == 2){ // already completed
+        URLType urlType = getNaverBlogURLType(inputURL);
+        if(urlType == URLType.PROCESSED){
             return inputURL;
         }
-        else if(urlType == 1){ // need to be processed
-            String result = "https://blog.naver.com/PostView.naver?blogId=";
+        else if(urlType == URLType.RAW){
+            String result = "";
             Matcher rawMatcher = this.rawPattern.matcher(inputURL);
             if(rawMatcher.find()){
                 String NAVER_ID = rawMatcher.group(1);
                 String POST_NUMBER = rawMatcher.group(2);
-                result += NAVER_ID + "&logNo=" + POST_NUMBER;
+                result = String.format("https://blog.naver.com/PostView.naver?blogId=%s&logNo=%s", NAVER_ID, POST_NUMBER);
             }
             return result;
         }
-        else{
+        else { // urlType == URLType.INVALID
             throw new Exception("잘못된 URL입니다");
         }
     }
