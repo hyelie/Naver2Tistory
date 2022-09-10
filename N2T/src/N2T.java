510c602b0e4cf5043a494ae0b447fd441df0a27f
hyelie <hyelie@postech.ac.kr>
2022-09-10 10:00:13 +0000

    Complete main function 메인함수 역할을 하는 N2T.java의 process 함수 수정  - 오류 메시지 수정  - 작업 중 access token이 만료되었을 때 최대 2회 로그인 재시도

--- N2T/src/N2T.java
+++ N2T/src/N2T.java
@@ -19,17 +19,24 @@ public class N2T {
     private Converter converter = new Converter();
 
     // 프로그램 실행 전에 config를 확인
-    private void init(){
+    private void initConfig(){
         try{
             // config 파일 읽어서 값 초기화
             System.out.println("[검증 중] : 기존에 입력한 사용자 정보를 확인 중입니다.");
             this.tistoryClient = new TistoryClient();
             
-
             // config 파일에 있는 값이 맞는지 확인 및 API가 정상작동하는지 테스트
             tistoryClient.authorize();
             System.out.println("[검증 완료] : 기존에 입력한 사용자 정보를 확인했습니다.");
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
+    // 프로그램 실행 전에 list.txt에 있는 url들을 읽어 옴.
+    private void initURLs(){
+        try{
             // 입력 파일에 있는 URL 입력받아 저장
             System.out.println("[파일 읽는 중] : list.txt 파일을 읽는 중입니다.");
             Path path = Paths.get(Util.getCurrentDirectory() + "/N2T/src/list.txt");
@@ -58,16 +65,15 @@ public class N2T {
 			String title = converter.getTitle();
             System.out.println("[작업 중] : " + title);
             
-			 // extract content from post
-			 converter.extractContent();
+			// extract content from post
+			converter.extractContent();
             
-			 // stylize content and get stylized result
-			 converter.stylize();
-			 String result = converter.getResult();
+			// stylize content and get stylized result
+			converter.stylize();
+			String result = converter.getResult();
 
-             // upload stylized result to tistory
-             tistoryClient.post("test", "test");
-            
+            // upload stylized result to tistory
+            tistoryClient.post(title, result);
         }
         catch(Exception e){
             System.out.println(e.getMessage());
@@ -76,11 +82,41 @@ public class N2T {
 
     // 프로그램 실행
     public void process(){
-        init();
-        //URLs.stream().forEach(element -> step(element));
+        // 초기화
+        try{
+            initConfig();
+            initURLs();
+        } catch(Exception e){
+            System.out.println("[종료] : 초기화 중 오류 발생으로 프로그램을 종료합니다.");
+            return;
+        }
+
+        System.out.println("[작업 시작] : Naver to Tistory 작업을 시작합니다.");
+        // 모든 URL에 대해 작업 진행
+        for(String URL : URLs){
+            int num_tries = 0, max_tries = 2;
+            while(true){
+                try{
+                    step(URL);
+                }
+                catch(Exception e){
+                    System.out.println("[작업 중 오류 발생] : 로그인을 재시도합니다.");
+                    initConfig();
+
+                    if(num_tries++ == max_tries){
+                        System.out.println("[검증 오류] : 사용자 정보 초기화가 반복적으로 실패해 프로그램을 종료합니다.");
+                        return;
+                    }                    
+                }
+            }
+        }
+
+        System.out.println("[작업 완료] : Naver to Tistory 작업이 성공적으로 끝났습니다.");
+        
+        String userBlogURL = "https://" + tistoryClient.getBlogName() + ".tistory.com/manage/posts";
+        Util.openWindow(userBlogURL);
         /*
         프로세싱 이후 로그 txt 형식으로 출력
-        티스토리 띄워줌
-         */
+        */
     }
 }
