1d8bcfed78f931bc405bb1e34ab573c40640ba7c
hyelie <hyelie@postech.ac.kr>
2022-09-11 12:42:04 +0000

    Make architecture of TDD classes

---
+++ N2T/src/test/ConverterTest.java
@@ -0,0 +1,6 @@
+package test;
+
+// converter가 제대로 작동하는지 검사해 주는 method가 있는 class
+public class ConverterTest {
+    
+}
---
+++ N2T/src/test/TistoryClientTest.java
@@ -0,0 +1,6 @@
+package test;
+
+// Tistory client의 auth가 제대로 작동하는지 검사해 주는 method가 있는 class
+public class TistoryClientTest {
+    
+}
