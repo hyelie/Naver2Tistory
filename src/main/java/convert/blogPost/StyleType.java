784e7c49aba1794850d2af1220d85e2898d130b0
hyelie <hyelie@postech.ac.kr>
2023-08-31 14:45:59 +0900

    BREAKING CHANGE: Rename SupportType to StyleType

---
+++ src/main/java/convert/blogPost/StyleType.java
@@ -0,0 +1,28 @@
+package convert.blogPost;
+
+public enum StyleType {
+    TABLE,
+        ROW,
+            COLUMN,
+    QUOTATION,
+        QUOTE,
+        CITE,
+    TEXT,
+        PARAGRAPH_DEFAULT,
+        PARAGRAPH_LEFT,
+        PARAGRAPH_RIGHT,
+        PARAGRAPH_CENTER,
+            CONTENT,
+                LINK,
+                BOLD,
+                TILT,
+                UNDERBAR,
+                STRIKE,
+    CODE,
+    IMAGE,
+        IMAGEBYTE,
+        CAPTION,
+    HORIZONTALLINE,
+    NONE;
+    // Append other types here to add other types of HTML formats.
+}
