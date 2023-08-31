79bd58414bdc0aaa06bae95535bd06ab88040103
hyelie <hyelie@postech.ac.kr>
2023-08-31 14:51:38 +0900

    cleanup: Reflect removed files

--- src/main/java/convert/blogPost/ConvertedTreeNode.java
+++ src/main/java/convert/blogPost/ConvertedTreeNode.java
@@ -1,6 +1,5 @@
 package convert.blogPost;
 
-import convert.SupportType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,12 +7,18 @@ import lombok.Getter;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+leaf node만 Node.content가 ""이 아니다.
+일반 텍스트는 [TEXT - PARAGRAPH - CONTENT - [style]] 순서로 이루어진다.
+만약 style이 있는 경우 CONTENT 아래에 style에 해당하는 node가 더 붙고,
+style이 없는 경우 CONTENT에 바로 내용이 붙는다.
+*/
 @Getter
 @Builder
 @AllArgsConstructor
 public class ConvertedTreeNode {
     @Builder.Default
-    private SupportType type = SupportType.NONE;
+    private StyleType type = StyleType.NONE;
 
     @Builder.Default
     private String content = "";
