e0fec92eb84cdc5430986ef46847df7177b4aca3
hyelie <hyelie@postech.ac.kr>
2023-09-01 21:58:02 +0900

    cleanup: Add convert folder in test code folder src/main과 동일한 폴더구성으로 만듬

---
+++ src/test/java/convert/scrapper/naver/sectionParsers/ImageParserTest.java
@@ -0,0 +1,79 @@
+package convert.scrapper.naver.sectionParsers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Test;
+
+import convert.blogPost.ConvertedTreeNode;
+import convert.blogPost.StyleType;
+import convert.scrappers.naver.SectionParser;
+import convert.scrappers.naver.sectionParsers.ImageParser;
+import utils.NodeTestUtils;
+
+public class ImageParserTest {
+    SectionParser imageParser = new ImageParser();
+
+    @Test
+    public void testImageParserWithCaption() throws Exception {
+        // given
+        String imageWithCaptionHtml =
+        "<div class=\"se-section se-section-image se-l-default se-section-align-\">" + 
+                "<div class=\"se-module se-module-image\" style=\"\">" + 
+                    "<a class=\"se-module-image-link __se_image_link __se_link\" style=\"\" onclick=\"return false;\" data-linktype=\"img\" data-linkdata=\"{&quot;id&quot; : &quot;SE-553d8212-fe88-41d2-a469-7f54c162240d&quot;, &quot;src&quot; : &quot;https://blogpfthumb-phinf.pstatic.net/20120504_73/jhi990823_1336138420833_S02En4_jpg/wallpaper-33614.jpg&quot;, &quot;originalWidth&quot; : &quot;1280&quot;, &quot;originalHeight&quot; : &quot;960&quot;, &quot;linkUse&quot; : &quot;false&quot;, &quot;link&quot; : &quot;&quot;}\" area-hidden=\"true\">" + 
+                        "<img src=\"https://blogpfthumb-phinf.pstatic.net/20120504_73/jhi990823_1336138420833_S02En4_jpg/wallpaper-33614.jpg\" alt=\"\" class=\"se-image-resource egjs-visible\">" + 
+                    "</a>" + 
+                "</div>" + 
+            "<div class=\"se-module se-module-text se-caption\"><p class=\"se-text-paragraph se-text-paragraph-align- \" style=\"\" id=\"SE-1d446049-4f57-4b0a-870d-47dc70e28a95\"><span style=\"\" class=\"se-fs- se-ff-   \" id=\"SE-789e0226-7cd8-48c1-ad35-fd7df407f27d\">고화질 이미지 캡션</span></p></div>" + 
+        "</div>";
+        Document doc = Jsoup.parse(imageWithCaptionHtml);
+        Element imageWithCaptionElement = doc.body().child(0);
+
+        //when
+        ConvertedTreeNode imageNode = imageParser.parseToTreeNode(imageWithCaptionElement);
+
+        // then
+        NodeTestUtils.assertNodeTypeAndContent(imageNode, StyleType.IMAGE, "");
+
+        ConvertedTreeNode imageByteNode = imageNode.getChilds().get(0);
+        assertNotNull(imageByteNode);
+        assertEquals(StyleType.IMAGEBASE64, imageByteNode.getType());
+
+        ConvertedTreeNode captionNode = imageNode.getChilds().get(1);
+        NodeTestUtils.assertNodeTypeAndContent(captionNode, StyleType.CAPTION, "");
+
+        ConvertedTreeNode contentNode = captionNode.getChilds().get(0).getChilds().get(0).getChilds().get(0);
+        NodeTestUtils.assertNodeTypeAndContent(contentNode, StyleType.CONTENT, "고화질 이미지 캡션");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testImageParserWithoutCaption() throws Exception {
+        // given
+        String imageWithCaptionHtml =
+        "<div class=\"se-section se-section-image se-l-default se-section-align-\">" + 
+                "<div class=\"se-module se-module-image\" style=\"\">" + 
+                    "<a class=\"se-module-image-link __se_image_link __se_link\" style=\"\" onclick=\"return false;\" data-linktype=\"img\" data-linkdata=\"{&quot;id&quot; : &quot;SE-553d8212-fe88-41d2-a469-7f54c162240d&quot;, &quot;src&quot; : &quot;https://blogpfthumb-phinf.pstatic.net/20120504_73/jhi990823_1336138420833_S02En4_jpg/wallpaper-33614.jpg&quot;, &quot;originalWidth&quot; : &quot;1280&quot;, &quot;originalHeight&quot; : &quot;960&quot;, &quot;linkUse&quot; : &quot;false&quot;, &quot;link&quot; : &quot;&quot;}\" area-hidden=\"true\">" + 
+                        "<img src=\"https://blogpfthumb-phinf.pstatic.net/20120504_73/jhi990823_1336138420833_S02En4_jpg/wallpaper-33614.jpg\" alt=\"\" class=\"se-image-resource egjs-visible\">" + 
+                    "</a>" + 
+                "</div>" + 
+        "</div>";
+        Document doc = Jsoup.parse(imageWithCaptionHtml);
+        Element imageWithCaptionElement = doc.body().child(0);
+
+        // when
+        ConvertedTreeNode result = imageParser.parseToTreeNode(imageWithCaptionElement);
+
+        // then
+        NodeTestUtils.assertNodeTypeAndContent(result, StyleType.IMAGE, "");
+
+        ConvertedTreeNode imageByteNode = result.getChilds().get(0);
+        assertNotNull(imageByteNode);
+        assertEquals(StyleType.IMAGEBASE64, imageByteNode.getType());
+
+        ConvertedTreeNode captionNode = result.getChilds().get(1);
+        NodeTestUtils.assertNodeTypeAndContent(captionNode, StyleType.CAPTION, "");
+    }
+}
---
+++ src/test/java/convert/scrapper/naver/sectionParsers/OglinkParserTest.java
@@ -0,0 +1,43 @@
+package convert.scrapper.naver.sectionParsers;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Test;
+
+import convert.blogPost.ConvertedTreeNode;
+import convert.blogPost.StyleType;
+import convert.scrappers.naver.SectionParser;
+import convert.scrappers.naver.sectionParsers.OglinkParser;
+import utils.NodeTestUtils;
+
+public class OglinkParserTest {
+    SectionParser oglinkParser = new OglinkParser();
+
+    @Test
+    public void testOglinkParser() throws Exception {
+        // given
+        String oglinkHtml =
+        "<div class=\"se-section se-section-oglink se-l-large_image se-section-align-\">" + 
+            "<div class=\"se-module se-module-oglink\">" + 
+                
+                "<a href=\"https://hyelie.tistory.com\" class=\"se-oglink-info\" target=\"_blank\">" + 
+                    "<div class=\"se-oglink-info-container\">" + 
+                        "<strong class=\"se-oglink-title\">Note</strong>" + 
+                        "<p class=\"se-oglink-summary\">소개소개</p>" + 
+                        "<p class=\"se-oglink-url\">hyelie.tistory.com</p>" + 
+                    "</div>" + 
+                "</a>" + 
+            "</div>" + 
+        "</div>";
+        Document doc = Jsoup.parse(oglinkHtml);
+        Element oglinkElement = doc.body().child(0);
+
+        // when
+        ConvertedTreeNode oglinkNode = oglinkParser.parseToTreeNode(oglinkElement);
+
+        NodeTestUtils.assertNodeTypeAndContent(oglinkNode, StyleType.NONE, "");
+    }
+}
+
+                        
